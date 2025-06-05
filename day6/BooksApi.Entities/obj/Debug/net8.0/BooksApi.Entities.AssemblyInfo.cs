3b493a1df6b3d64d800927499b432fa4eb64ed7b
kreshipatel <kreshiviroja487@gmail.com>
2025-06-05 19:39:19 +0530

    day7 upload

--- day6/BooksApi.Entities/obj/Debug/net8.0/BooksApi.Entities.AssemblyInfo.cs
+++ day6/BooksApi.Entities/obj/Debug/net8.0/BooksApi.Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BooksApi.Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d6be23cadcfbaa32f96bad617259d5b39c35bc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BooksApi.Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BooksApi.Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
